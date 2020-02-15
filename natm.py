8f7606a44e6b1eb7a38224a8a2886821d9381f7a
vinh40 <vinh40.nguyen@gmail.com>
2020-02-15 15:21:34 -0500

    Update natm.py

--- natm.py
+++ natm.py
@@ -1,5 +1,55 @@
+
+
+
+class addcredit():
+	def add():
+		print ("Does something")
+		
+	def request():
+		print ("do more")
+
+class sell():
+	def sell():
+		print ("Does something")
+
+class buy():
+	def buy():
+		print ("Does something")
+
+class createU():
+	def creates():
+		print ("Does something")
+		
+	def unique():
+		print ("do more")
+
+class deleteU():
+	def exists():
+		print ("Does something")
+		
+	def delete():
+		print ("do more")
+
+class login():
+	def getAccounts():
+		print ("Does something")
+		
+class logout():
+	def write_file():
+		print ("Does something")
+
+class file_update():
+	def update_atf():
+		print ("Does something")
+	
+	def update_uaf():
+		print ("do more")
+
+
 def main(): 
 	print("hello world")
+	
+	print ("hello world")
 	
 if __name__ == '__main__':
 	main()
