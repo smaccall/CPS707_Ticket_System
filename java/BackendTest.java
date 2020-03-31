6c62822992119cfdca074e937a32b8d1e4d3230c
vinh40 <vinh40.nguyen@gmail.com>
2020-03-31 19:25:12 -0400

    new stuff

--- java/BackendTest.java
+++ java/BackendTest.java
@@ -1,18 +1,28 @@
 import jdk.jfr.StackTrace;
-import org.junit.Test;
+import org.apache.commons.io.FileUtils;
+import org.junit.Assert;
+import org.junit.*;
+import java.io.*;
+
 import static org.junit.Assert.*;
 
 public class BackendTest {
-    public void testAdd(){
-        //Updater update = new Updater();
+    public void testAdd() throws IOException {
+        Updater update = new Updater();
+        File dir = new File("/output/");
+        File[] dirList = dir.listFiles();
+        if (dirList != null){
+            for (File output : dirList){
+                //update.addC();
+                File input = new File("/input/"+ output.getName());
+                Assert.assertEquals(FileUtils.readLines(input), FileUtils.readLines(output));
 
-        //Input and output files i think should go here
-        final File expected = new File();
-        final File output =  File();
+            }
+        }
         //Input file to be tested in the updater method
 
         //this compares maybe im just doing alot of googling
-        Assert.assertEquals(FileUtils.readlines(expected), FileUtils.readlines(output));
+
 
     }
 }
