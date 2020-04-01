7e73f71e5e277b9f0f7f0f0e0895f1bfdf6a08d4
vinh40 <vinh40.nguyen@gmail.com>
2020-04-01 17:57:03 -0400

    sure

--- java/BackendTest.java
+++ java/BackendTest.java
@@ -1,33 +1,99 @@
-import jdk.jfr.StackTrace;
 import org.apache.commons.io.FileUtils;
 import org.junit.Assert;
-import org.junit.*;
+
 import java.io.*;
 
-import static org.junit.Assert.*;
 
 public class BackendTest {
-    public void testAdd() throws IOException {
-        Main main = new Main();
-        File dir = new File("/output/");
-        File[] dirList = dir.listFiles();
+    public static void main(String[] args) throws IOException {
+        undo();
+        testAdd();
+        undo();
+        testBuy();
+        undo();
+        testSell();
+        undo();
+        testCreate();
+        undo();
+        testDelete();
+        undo();
+        testRefund();
+        //testModify();
+    }
+
+    public static void testAdd() throws IOException {
         Updater update = new Updater();
         update.addC("06 username2       BS 001000.00");
-
         //statement coverage
-        Assert.assertEquals(FileUtils.readLines(new File("add1_uad_EO.txt"), "UTF-8"),
-                FileUtils.readLines(new File("placehold2.txt"), "UTF-8"));
+        Assert.assertEquals(FileUtils.readLines(new File("java/output/add1_uaf_EO.txt"), "UTF-8"),
+                FileUtils.readLines(new File("src/uaf.txt"), "UTF-8"));
     }
 
-    public void testBuy(){
+    public static void testBuy() throws IOException {
+        Updater update = new Updater();
+        //statement coverage
+        update.buy("04 Concert3             username3     094 300.00");
+        Assert.assertEquals(FileUtils.readLines(new File("java/output/buy1_uaf_EO.txt"), "UTF-8"),
+                FileUtils.readLines(new File("src/uaf.txt"), "UTF-8"));
 
+        Assert.assertEquals(FileUtils.readLines(new File("java/output/buy1_atf_EO.txt"), "UTF-8"),
+                FileUtils.readLines(new File("src/atf.txt"), "UTF-8"));
     }
 
-    public void testBuy(){
 
+    public static void testSell() throws IOException{
+        Updater update = new Updater();
+        update.sell("04 Little House 5      username1      009 015.00");
+        Assert.assertEquals(FileUtils.readLines(new File("java/output/sell1_atf_EO.txt"), "UTF-8"),
+                FileUtils.readLines(new File("src/atf.txt"), "UTF-8"));
     }
 
-    public void testBuy(){
+
+    public static void testCreate() throws IOException{
+        Updater update = new Updater();
+        update.create("01 usertest321     BS 000500.00");
+        Assert.assertEquals(FileUtils.readLines(new File("java/output/create1_uaf_EO.txt"), "UTF-8"),
+                FileUtils.readLines(new File("src/uaf.txt"), "UTF-8"));
+    }
+
+    public static void testDelete() throws IOException{
+        Updater update = new Updater();
+        update.delete("02 username1       FS 999000.00");
+        Assert.assertEquals(FileUtils.readLines(new File("java/output/delete1_uaf_EO.txt"), "UTF-8"),
+                FileUtils.readLines(new File("src/uaf.txt"), "UTF-8"));
+
+        Assert.assertEquals(FileUtils.readLines(new File("java/output/delete1_atf_EO.txt"), "UTF-8"),
+                FileUtils.readLines(new File("src/atf.txt"), "UTF-8"));
+    }
+
+    public static void testRefund() throws IOException{
+        Updater update = new Updater();
+        update.refund("05 username2      username3      000200.00");
+        Assert.assertEquals(FileUtils.readLines(new File("java/output/refund1_uaf_EO.txt"), "UTF-8"),
+                FileUtils.readLines(new File("src/uaf.txt"), "UTF-8"));
+    }
+
+    public static void testModify() throws IOException {
+        Updater update = new Updater();
+        update.modifyFile("src/uaf.txt", "username2       BS 001000.00", "username2       BS 002000.00");
+    }
+
+    public static void undo() throws IOException {
+        FileWriter fw = new FileWriter("src/uaf.txt");
+        fw.write("username1       FS 999000.00\n");
+        fw.write("username2       BS 001000.00\n");
+        fw.write("username3       SS 000500.99\n");
+        fw.write("username4       AA 500000.00\n");
+        fw.write("admin1          AA 001500.00\n");
+        fw.write("nomoney         FS 000000.00\n");
+        fw.close();
+
+        fw = new FileWriter("src/atf.txt");
+        fw.write("Concert1            username4      030 050.00\n");
+        fw.write("Concert2            username1      079 250.00\n");
+        fw.write("Concert3            username3      100 300.00\n");
+        fw.write("END");
+        fw.close();
 
     }
 }
--- java/Main.java
+++ java/Main.java
@@ -1,291 +1,265 @@
+import org.apache.commons.io.FileUtils;
+
 import java.io.*;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class Main {
 
-	/**
-	 * @param args
-	 * @throws IOException 
-	 */
-	public static void main(String[] args) throws IOException {
-		// Reads through daily transaction file to apply changes to uaf and atf
-		merger();
-		try {
-			Scanner scanner = new Scanner(new File("dtf.txt"));
-			while (scanner.hasNextLine()) {
-				String hold = scanner.nextLine();
-				Updater up = new Updater();
-				System.out.println(hold);
-				if (hold.length() > 0) {
-					if (hold.substring(0, 2).equals("01")) {
-						up.create(hold);
-					} else if (hold.substring(0, 2).equals("02")) {
-						up.delete(hold);
-					} else if (hold.substring(0, 2).equals("03")) {
-						up.sell(hold);
-					} else if (hold.substring(0, 2).equals("04")) {
-						up.buy(hold);
-					} else if (hold.substring(0, 2).equals("05")) {
-						up.refund(hold);
-					} else if (hold.substring(0, 2).equals("06")) {
-						up.addC(hold);
-					} else if (hold.substring(0, 2).equals("00")) {
-						System.out.println("WAAAA2");
-					} else {
-						System.out.println("WAAAA");
-					}
-				}
-			}
-			scanner.close();
-		} catch (FileNotFoundException ex) {
-			System.out.print("File not found");
-		}
-	}
+    /**
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+        // Reads through daily transaction file to apply changes to uaf and atf
+        merger();
+        try {
+            Scanner scanner = new Scanner(new File("dtf.txt"));
+            while (scanner.hasNextLine()) {
+                String hold = scanner.nextLine();
+                Updater up = new Updater();
+                System.out.println(hold);
+                if (hold.length() > 0) {
+                    if (hold.substring(0, 2).equals("01")) {
+                        up.create(hold);
+                    } else if (hold.substring(0, 2).equals("02")) {
+                        up.delete(hold);
+                    } else if (hold.substring(0, 2).equals("03")) {
+                        up.sell(hold);
+                    } else if (hold.substring(0, 2).equals("04")) {
+                        up.buy(hold);
+                    } else if (hold.substring(0, 2).equals("05")) {
+                        up.refund(hold);
+                    } else if (hold.substring(0, 2).equals("06")) {
+                        up.addC(hold);
+                    } else if (hold.substring(0, 2).equals("00")) {
+                        System.out.println("WAAAA2");
+                    } else {
+                        System.out.println("WAAAA");
+                    }
+                }
+            }
+            scanner.close();
+        } catch (FileNotFoundException ex) {
+            System.out.print("File not found");
+        }
+    }
 
-	/**
-	 * @param args
-	 * @throws IOException 
-	 * merge multiple daily transaction files into one
-	 */
-	public static void merger() throws IOException {
-		File dir = new File("src/output");
-		PrintWriter pw = new PrintWriter("dtf.txt"); 
-		String[] fileNames = dir.list();
+    /**
+     * @param args
+     * @throws IOException merge multiple daily transaction files into one
+     */
+    public static void merger() throws IOException {
+        File dir = new File("src/output");
+        PrintWriter pw = new PrintWriter("dtf.txt");
+        String[] fileNames = dir.list();
 
-		for (String fileName : fileNames) {
-			if (fileName.contains("dtf")) {
-				File f = new File(dir, fileName); 	  
-				// create object of BufferedReader 
-				BufferedReader br = new BufferedReader(new FileReader(f));
-				// Read from current file 
-				String line = br.readLine(); 
-				while (line != null) { 
-					// write to the output file 
-					pw.println(line); 
-					line = br.readLine(); 
-				} 
-				pw.flush(); 
-				br.close();
-			}
-		}
-		pw.close();
-	}
+        for (String fileName : fileNames) {
+            if (fileName.contains("dtf")) {
+                File f = new File(dir, fileName);
+                // create object of BufferedReader
+                BufferedReader br = new BufferedReader(new FileReader(f));
+                // Read from current file
+                String line = br.readLine();
+                while (line != null) {
+                    // write to the output file
+                    pw.println(line);
+                    line = br.readLine();
+                }
+                pw.flush();
+                br.close();
+            }
+        }
+        pw.close();
+    }
 }
 
 /**
  * Class that has methods to update each file according to the correct action
  */
 class Updater {
-	/**
-	 * @param input
-	 */
-	public void addC(String input) throws  IOException {
-		// update the uaf file with the new ammount of credit for that user
-		String nameB = input.substring(3,19).strip();
-		String roleB = input.substring(19,22).strip();
-		String amountB = input.substring(22).strip();
-		Scanner s = new Scanner((new File("src/uaf.txt")));
-		FileWriter fw = new FileWriter("src/uaf.txt");
-		while (s.hasNextLine()){
-			String output = s.nextLine();
-			if (output.substring(0,16).equals(nameB)){
-				Float total = Float.parseFloat(output.substring(20,29)) + Float.parseFloat(amountB);
-				fw.write(String.format("%-15d", nameB) + " " + roleB + " " + String.format("%09d", Float.toString(total)));
-			} else {
-				fw.write(output);
-			}
-		}
-		fw.close();
-		s.close();
-	}
-	/**
-	 * @param input
-	 * @throws IOException 
-	 */
-	public void buy(String in) throws IOException{
-		// update the atf file with the new amount of available tickets
-		// update the uaf with buyers new credit amount
-		Scanner s = new Scanner(new File("src/atf.txt"));
-		String eTitle = in.substring(3,22).strip();
-		String sName = in.substring(23,37).strip();
-		String numTicketsStr = in.substring(38,41).strip();
-		int numTickets = Integer.parseInt(numTicketsStr);
-		String newLine = "";
-		while (s.hasNextLine()) {
-			String temp = s.nextLine();
-			String atfETitle = temp.substring(0,19).strip();
-			String atfSName = temp.substring(20,34).strip();
-			if (atfETitle.equals(eTitle) && atfSName.equals(sName)) {
-				String atfnumTicketsStr = temp.substring(35,38).strip();
-				int atfnumTickets = Integer.parseInt(atfnumTicketsStr);
-				String atfpriceTicketStr = temp.substring(39,45).strip();
-				int newTotal = atfnumTickets - numTickets;
-				newLine = atfETitle + atfSName + newTotal + atfpriceTicketStr;
-				break;
-			}
-		}
-		Updater.modifyFile("src/atf.txt", in, newLine);
-		s.close();
+    /**
+     * @param input
+     */
+    public void addC(String input) throws IOException {
+        // update the uaf file with the new ammount of credit for that user
+        String nameB = input.substring(3, 19).strip();
+        String roleB = input.substring(19, 22).strip();
+        String amountB = input.substring(22).strip();
+        Scanner s = new Scanner((new File("src/uaf.txt")));
+        while (s.hasNextLine()) {
+            String output = s.nextLine();
+            if (output.substring(0, 16).strip().equals(nameB)) {
+                Float total = Float.parseFloat(output.substring(20)) + Float.parseFloat(amountB);
+                String news = String.format("%-15s", nameB) + " " + roleB + " " + (String.format("%09.2f", total));
+                modifyFile("src/uaf.txt", output, news);
+                break;
+            }
+        }
+        s.close();
+    }
 
-		s = new Scanner(new File("uaf.txt"));
-		sName = in.substring(23,37).strip();
-		numTicketsStr = in.substring(38,41).strip();
-		numTickets = Integer.parseInt(numTicketsStr);
-		String priceTicketStr = in.substring(42,48).strip();
-		int priceTicket = Integer.parseInt(priceTicketStr); 
-		int totalCredit = numTickets * priceTicket; 
-		newLine = "";
-		while (s.hasNextLine()) {
-			String temp = s.nextLine();
-			String uafSName = temp.substring(0,15).strip();
-			if (uafSName.equals(sName)) {
-				String uafType = temp.substring(16,18).strip();
-				String uafCreditsStr = temp.substring(20,28).strip();
-				int uafCredits = Integer.parseInt(uafCreditsStr);
-				int newTotal = uafCredits + totalCredit;
-				newLine = uafSName + uafType + newTotal;
-				break;
-			}
-		}
-		Updater.modifyFile("uaf.txt", in, newLine);
-		s.close();
+    /**
+     * @param input
+     * @throws IOException
+     */
+    public void buy(String in) throws IOException {
+        // update the atf file with the new amount of available tickets
+        // update the uaf with buyers new credit amount
+        Scanner s = new Scanner(new File("src/atf.txt"));
+        String eTitle = in.substring(3, 22).strip();
+        String sName = in.substring(23, 37).strip();
+        String numTicketsStr = in.substring(38, 41).strip();
+        int numTickets = Integer.parseInt(numTicketsStr);
+        String newLine = "";
+        int atfnumTickets = 0;
+        String temp = "";
+        while (s.hasNextLine()) {
+            temp = s.nextLine();
+            String atfETitle = temp.substring(0, 19).strip();
+            String atfSName = temp.substring(20, 34).strip();
+            if (atfETitle.equals(eTitle) && atfSName.equals(sName)) {
+                String atfnumTicketsStr = temp.substring(35, 38).strip();
+                atfnumTickets = Integer.parseInt(atfnumTicketsStr);
+                String atfpriceTicketStr = temp.substring(39, 45).strip();
+                //int newTotal = atfnumTickets - numTickets;
 
-	}
+                newLine = String.format("%-19s", atfETitle) + " " + String.format("%-14s", atfSName) +
+                        " " + String.format("%03d", numTickets) + " " + atfpriceTicketStr;
 
-	/**
-	 * @param input
-	 * @throws IOException 
-	 */
-	public void sell(String in) throws IOException{
-		// update the atf file with the new ticket available for sale
-		try {
-			Scanner s = new Scanner(new File("src/atf.txt"));
-			String eTitle = in.substring(3,22).strip();
-			String sName = in.substring(23,37).strip();
-			String numTicketsStr = in.substring(38,41).strip();
-			int numTickets = Integer.parseInt(numTicketsStr);
-			String newLine = "";
-			while (s.hasNextLine()) {
-				String temp = s.nextLine();
-				String atfETitle = temp.substring(0,19).strip();
-				String atfSName = temp.substring(20,34).strip();
-				if (atfETitle.equals(eTitle) && atfSName.equals(sName)) {
-					String atfnumTicketsStr = temp.substring(35,38).strip();
-					int atfnumTickets = Integer.parseInt(atfnumTicketsStr);
-					String atfpriceTicketStr = temp.substring(39,45).strip();
-					int newTotal = atfnumTickets + numTickets;
-					newLine = atfETitle + atfSName + newTotal + atfpriceTicketStr;
-					break;
-				}
-			}
-			Updater.modifyFile("src/atf.txt", in, newLine);
-			s.close();
-		}
-		catch (FileNotFoundException ex) {
-			System.out.print("ERROR: <File not found>");
-		}
-	}
+                break;
+            }
+        }
+        Updater.modifyFile("src/atf.txt", temp, newLine);
+        s.close();
 
-	/**
-	 * @param input
-	 */
-	public void create(String input) throws IOException{
-		// update uaf file with the newly created user
-		String nameB = input.substring(3,19).strip();
-		String role = input.substring(19,22).strip();
-		String credit = input.substring(22).strip();
-		Scanner s = new Scanner((new File("src/uaf.txt")));
-		FileWriter fw = new FileWriter("src/uaf.txt");
-		while (s.hasNext()) {
-			//get to bottom of file.
-		}
-		fw.write(String.format("%-15s", nameB) + " " + role + " " + credit);
-		s.close();
-		fw.close();
-	}
+        s = new Scanner(new File("src/uaf.txt"));
+        sName = in.substring(23, 37).strip();
+        numTicketsStr = in.substring(38, 41).strip();
+        numTickets = Integer.parseInt(numTicketsStr);
+        String priceTicketStr = in.substring(42, 48).strip();
+        float priceTicket = Float.parseFloat(priceTicketStr);
+        float totalCredit = (atfnumTickets - numTickets) * priceTicket;
+        newLine = "";
+        while (s.hasNextLine()) {
+            temp = s.nextLine();
+            String uafSName = temp.substring(0, 15).strip();
+            if (uafSName.equals(sName)) {
+                String uafType = temp.substring(16, 18).strip();
+                String uafCreditsStr = temp.substring(20, 28).strip();
+                float uafCredits = Float.parseFloat(uafCreditsStr);
+                float newTotal = uafCredits + totalCredit;
 
-	/**
-	 * @param input
-	 */
-	public void delete(String input) throws IOException{
-		// delete account from uaf and any related tickets deleted from atf
-		String nameB = input.substring(3,19).strip();
-		Scanner s = new Scanner((new File("src/uaf.txt")));
-		FileWriter fw = new FileWriter("src/uaf.txt");
-		Scanner s2 = new Scanner((new File("src/atf.txt")));
-		FileWriter fw2 = new FileWriter("src/atf.txt");
-		while (s.hasNextLine()){
-			String output = s.nextLine();
-			if (!(output.substring(0,16).equals(nameB))){
-				fw.write(output);
-			} 
-		}
-		while (s2.hasNextLine()) {
-			String output2 = s2.nextLine();
-			if (!output2.substring(21,34).equals(nameB))
-				fw2.write(output2);
-		}
-		fw.close();
-		s.close();
-		s2.close();
-		fw2.close();
-	}
+                newLine = String.format("%-15s", uafSName) + " " + uafType + " " + (String.format("%09.2f", newTotal));
+                break;
+            }
+        }
+        Updater.modifyFile("src/uaf.txt", temp, newLine);
+        s.close();
+    }
 
-	/**
-	 * @param input
-	 */
-	public void refund(String in) throws IOException {
-		// update buyer and sellers credit amount in uaf
-		String buyer = in.substring(3,19).strip();
-		String seller = in.substring(19,35).strip();
-		String credit = in.substring(35).strip();
+    /**
+     * @param input
+     * @throws IOException
+     */
+    public void sell(String in) throws IOException {
+        // update the atf file with the new ticket available for sale
+        Updater.modifyFile("src/atf.txt", "END", in.substring(3) + "\nEND");
 
-		Scanner s = new Scanner((new File("src/uaf.txt")));
-		FileWriter fw = new FileWriter("src/uaf.txt");
+    }
 
-		while (s.hasNextLine()) {
-			String line = s.nextLine();
-			String name = line.substring(0,16).strip();
-			if (name.equalsIgnoreCase(buyer)) {
-				String role = line.substring(16,19).strip();
-				float total = Float.parseFloat(line.substring(20,29)) + Float.parseFloat(credit);
-				fw.write(String.format("%-15d", buyer) + " " + role + " " + String.format("%09d", Float.toString(total)));
-			} else if (name.equalsIgnoreCase(seller)) {
-				String role = line.substring(16,19).strip();
-				float total = Float.parseFloat(line.substring(20,29)) - Float.parseFloat(credit);
-				fw.write(String.format("%-15d", seller) + " " + role + " " + String.format("%09d", Float.toString(total)));
-			}
-		}
-		s.close();
-		fw.close();
-	}
+    /**
+     * @param input
+     */
+    public void create(String input) throws IOException {
+        // update uaf file with the newly created user
+        BufferedWriter writer = new BufferedWriter(
+                new FileWriter("src/uaf.txt", true)  //Set true for append mode
+        );
+        writer.write(input.substring(3));
+        writer.close();
+    }
 
-	/**
-	 * @param filePath, oldString, newString
-	 */
-	static void modifyFile(String filePath, String oldString, String newString) throws IOException {
-		// modify the file as specified
-		File fileToBeModified = new File(filePath); 
-		String oldContent = "";
-		BufferedReader reader = null;
-		FileWriter writer = null;
+    /**
+     * @param input
+     */
+    public void delete(String input) throws IOException {
+        // delete account from uaf and any related tickets deleted from atf
 
-		reader = new BufferedReader(new FileReader(fileToBeModified));
-		//Reading all the lines of input text file into oldContent
-		String line = reader.readLine();
-		while (line != null) {
-			oldContent = oldContent + line + System.lineSeparator();
-			line = reader.readLine();
-		}
+        List<String> lines = FileUtils.readLines(new File("src/uaf.txt"));
+        List<String> updatedLines = lines.stream().filter(s -> !s.contains(input.substring(3))).collect(Collectors.toList());
+        FileUtils.writeLines(new File("src/uaf.txt"), updatedLines, false);
 
-		//Replacing oldString with newString in the oldContent  
-		String newContent = oldContent.replaceAll(oldString, newString);
+        lines = FileUtils.readLines(new File("src/atf.txt"));
+        updatedLines = lines.stream().filter(s -> !s.contains(input.substring(3,19).strip())).collect(Collectors.toList());
+        FileUtils.writeLines(new File("src/atf.txt"), updatedLines, false);
+    }
 
-		//Rewriting the input text file with newContent
-		writer = new FileWriter(fileToBeModified);
-		writer.write(newContent);
+    /**
+     * @param input
+     */
+    public void refund(String in) throws IOException {
+        // update buyer and sellers credit amount in uaf
+        String buyer = in.substring(3, 18).strip();
+        String seller = in.substring(18, 33).strip();
+        String credit = in.substring(33).strip();
+        String oldbuy = "";
+        String oldsell = "";
+        String buys = "";
+        String sells = "";
 
-		reader.close();    
-		writer.close();
+        Scanner s = new Scanner((new File("src/uaf.txt")));
+        while (s.hasNextLine()) {
+            String line = s.nextLine();
+            String name = line.substring(0, 16).strip();
+            if (name.equalsIgnoreCase(buyer)) {
+                String role = line.substring(16, 19).strip();
+                float total = Float.parseFloat(line.substring(20)) + Float.parseFloat(credit);
+                oldbuy = line;
+                buys = String.format("%-15s", buyer) + " " + role + " " + String.format("%09.2f", (total));
+            } else if (name.equalsIgnoreCase(seller)) {
+                String role = line.substring(16, 19).strip();
+                float total = Float.parseFloat(line.substring(20)) - Float.parseFloat(credit);
+                oldsell = line;
+                sells = String.format("%-15s", seller) + " " + role + " " + String.format("%09.2f", (total));
 
-	}
+            }
+        }
+        modifyFile("src/uaf.txt", oldbuy,
+                buys);
+        modifyFile("src/uaf.txt", oldsell,
+                sells);
+        s.close();
+    }
+
+    /**
+     * @param filePath, oldString, newString
+     */
+    static void modifyFile(String filePath, String oldString, String newString) throws IOException {
+        // modify the file as specified
+        File fileToBeModified = new File(filePath);
+        String oldContent = "";
+        BufferedReader reader = null;
+        FileWriter writer = null;
+
+        reader = new BufferedReader(new FileReader(fileToBeModified));
+        //Reading all the lines of input text file into oldContent
+        String line = reader.readLine();
+        while (line != null) {
+            oldContent = oldContent + line + System.lineSeparator();
+            line = reader.readLine();
+        }
+
+        //Replacing oldString with newString in the oldContent
+        String newContent = oldContent.replaceAll(oldString, newString);
+
+        //Rewriting the input text file with newContent
+        writer = new FileWriter(fileToBeModified);
+        writer.write(newContent);
+
+        reader.close();
+        writer.close();
+
+    }
 }
